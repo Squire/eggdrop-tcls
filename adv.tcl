23191d5ec11a6b309b8d1e92dcd6e5d448a4d1aa
Squire <curse@orcon.net.nz>
2013-10-24 00:44:11 +1300

    test

--- adv.tcl
+++ adv.tcl
@@ -3,5 +3,5 @@ bind pub - ".adv" time_adv
 bind time - "?0 * * * *" time_adv
 
 proc time_adv {min hour day month year} {
- putserv "PRIVMSG #shells \0031,15 • \002Shell Solutions\002 • High Performance Internet Solutions • \002http://www.shellsolutions.net\002 • Fast Customer Support ~ Instant Setup •"
+	putserv "PRIVMSG #shells \0031,15 • \002Shell Solutions\002 • High Performance Internet Solutions • \002http://www.shellsolutions.net\002 • Fast Customer Support ~ Instant Setup •"
 }
